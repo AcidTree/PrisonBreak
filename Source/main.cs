035aad27402ca71decc0059aa6a0cbb872649d90
nigel <>
2024-04-20 16:46:38 +1000

    version 1.0

---
+++ Source/main.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+
+namespace PrisonBreaks
+{
+    [StaticConstructorOnStartup]
+    public static class main
+    {
+        static main()
+        {
+            var harmony = new Harmony("com.lessprisonbreaks");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("Prison breaks patched");
+        }
+
+        [HarmonyPatch(typeof(PrisonBreakUtility), nameof(PrisonBreakUtility.CanParticipateInPrisonBreak))]
+        class Patch
+        {
+            [HarmonyPrefix]
+            static bool Prefix(ref bool __result, Pawn pawn)
+            {
+                if (MoodThresholdExtensions.CurrentMoodThresholdFor(pawn) == MoodThreshold.None)
+                {
+                    __result = false;
+#if DEBUG
+                    Log.Message("patching prison break for: " + pawn.Name.ToString());
+#endif
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
